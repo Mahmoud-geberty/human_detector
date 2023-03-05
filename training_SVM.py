a368e0721d4d7138124355443bf01ed084aa9e90
Mahmoud Abdelwase <opssheesh@gmail.com>
2023-03-04 21:00:57 -0800

    initial commit

---
+++ training_SVM.py
@@ -0,0 +1,57 @@
+from skimage.feature import hog
+#from skimage.io import imread
+import joblib,glob,os,cv2
+
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn import svm, metrics
+import numpy as np 
+from sklearn.preprocessing import LabelEncoder
+
+train_data = []
+train_labels = []
+pos_im_path = 'DATAIMAGE/positive/'
+neg_im_path = 'DATAIMAGE/negative/'
+model_path = 'models/models.dat'
+# Load the positive features
+for filename in glob.glob(os.path.join(pos_im_path,"*.png")):
+    fd = cv2.imread(filename,0)
+    fd = cv2.resize(fd,(64,128))
+    fd = hog(fd,block_norm='L2',orientations=9,pixels_per_cell=(8,8),visualize=False,cells_per_block=(2,2))
+    train_data.append(fd)
+    train_labels.append(1)
+
+len1 = len(train_data)
+
+# Load the negative features
+for filename in glob.glob(os.path.join(neg_im_path,"*.jpg")):
+    fd = cv2.imread(filename,0)
+    fd = cv2.resize(fd,(64,128))
+    fd = hog(fd,block_norm='L2',orientations=9,pixels_per_cell=(8,8),visualize=False,cells_per_block=(2,2))
+    train_data.append(fd)
+    train_labels.append(0)
+
+len2 = len(train_data) - len1
+
+
+train_data = np.float32(train_data)
+train_labels = np.array(train_labels)
+
+print('Data Prepared........')
+print(f" positive samples:{len1}")
+print(f" negative samples:{len2}")
+print('Train Data:',len(train_data))
+print('Train Labels (1,0)',len(train_labels))
+print("""
+Classification with SVM
+
+""")
+
+
+#model = LinearSVC(max_iter=10000,  tol=0.00000001)
+model = LinearSVC(C=0.005, max_iter=4000, dual=False, tol=0.0000000001)
+print('Training...... Support Vector Machine')
+model.fit(train_data,train_labels)
+
+joblib.dump(model, 'models/models.dat')
+print('Model saved : {}'.format('models/models.dat'))
---
+++ visualize_image.py
@@ -0,0 +1,75 @@
+import time
+import numpy as np 
+import cv2,joblib,glob,os
+import Sliding as sd
+from imutils.object_detection import non_max_suppression
+import imutils
+from skimage.feature import hog
+from skimage import color
+from skimage.transform import pyramid_gaussian
+
+test_path =  'test/'
+#test_path = 'DATAIMAGE/positive/'
+
+for filename in glob.glob(os.path.join(test_path, "*")):
+
+	image = cv2.imread(filename)
+	image = cv2.resize(image,(400,256))
+	size = (64,128)
+	step_size = (8,8)
+	downscale = 1.10
+#List to store the detections
+	detections = []
+#The current scale of the image 
+	scale = 0
+	model = joblib.load('models/models.dat')
+
+	first_iter = 1
+
+	for im_scaled in pyramid_gaussian(image, downscale = downscale, channel_axis=-1):
+		#The list contains detections at the current scale
+		if im_scaled.shape[0] < size[1] or im_scaled.shape[1] < size[0]:
+			break
+		for (x, y, window) in sd.sliding_window(im_scaled, size, step_size):
+			if window.shape[0] != size[1] or window.shape[1] != size[0]:
+				continue
+			window = color.rgb2gray(window)
+			if (first_iter):
+				st_hog = time.process_time_ns()
+			fd=hog(window, block_norm='L2',orientations=9,pixels_per_cell=(8,8),visualize=False,cells_per_block=(2,2))
+			if (first_iter):
+				et_hog = time.process_time_ns()
+			fd = fd.reshape(1, -1)
+			if (first_iter):
+				st_svm = time.process_time_ns()
+			pred = model.predict(fd)
+			if (first_iter):
+				first_iter = 0
+				et_svm = time.process_time_ns()
+			if pred == 1:
+					
+				if model.decision_function(fd) > 0.5:
+					detections.append((int(x * (downscale**scale)), int(y * (downscale**scale)), model.decision_function(fd), 
+					int(size[0] * (downscale**scale)),
+					int(size[1] * (downscale**scale))))
+	 
+		scale += 1
+
+	clone = image.copy()
+	rects = np.array([[x, y, x + w, y + h] for (x, y, _, w, h) in detections])
+	sc = [score[0] for (x, y, score, w, h) in detections]
+#print ("sc: ", sc)
+	sc = np.array(sc)
+	pick = non_max_suppression(rects, probs = sc, overlapThresh = 0.30)
+	for(x1, y1, x2, y2) in pick:
+		cv2.rectangle(clone, (x1, y1), (x2, y2), (0, 255, 0), 2)
+		cv2.putText(clone,'Person',(x1-2,y1-2),1,0.75,(121,12,34),1)
+	hog_time = et_hog - st_hog
+	svm_time = et_svm - st_svm
+	print(f"hog cpu time per iteration in ns: {hog_time}")
+	print(f"svm cpu time per iteration in ns: {svm_time}")
+	print(f"hog takes {hog_time/svm_time} more time than svm classification")
+
+	cv2.imshow('Person Detection',clone)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
